e110896df7788ea6b56b5871b58bd7b0f451408f
Brandon Mulcahy <brandon@lightcones.net>
2018-07-19 11:07:44 -0400

    Look up path to command-line arg instead of a hard-coded node

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"container/list"
 	"log"
+	"os"
 )
 
 func main() {
 	r := NewRoute()
 	path := list.New()
-	target := r.Map["kill facade"]
+	target := r.Map[os.Args[1]]
 	mark := target.GetMark(path)
 	if mark == MarkTrue {
 		for path.Len() > 0 {
