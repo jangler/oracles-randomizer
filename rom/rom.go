9ad494802e70617df8b6755538699723ab0ba397
Brandon Mulcahy <brandon@lightcones.net>
2018-07-20 05:28:23 -0400

    Replace bank variables with raw ints

--- rom/rom.go
+++ rom/rom.go
@@ -2,37 +2,28 @@
 // addresses are for the Japanese version of the game.
 package rom
 
-// A Bank corresponds to an MBC5 memory bank offset, labeled by the BGB
-// reckoning.
-type Bank int
-
-// only banks which are used in the randomizer are defined
-const (
-	ROM4 Bank = 0x0c000
-	ROM8 Bank = 0x1c000
-	RO15 Bank = 0x50000
-)
-
 // A Mutable is a byte that can be changed by the randomizer. Addr is the
 // offset within the bank, Old is the original value (for validation purposes),
 // and New is the replacement value.
 type Mutable struct {
-	Bank     Bank
-	Addr     int
-	Old, New byte
+	Bank, Addr int // each bank is a 0x4000-byte offset, starting at 2
+	Old, New   byte
 }
 
 // code/data positions
 var (
 	// want to have maku gate open from start
-	MakuGateCheck = Mutable{ROM4, 0x6a13, 0x7e, 0x66}
+	MakuGateCheck = Mutable{0x04, 0x6a13, 0x7e, 0x66}
 
 	// want to have the horon village shop stock *and* sell items from the
 	// start; replace each with $02
-	HoronShopStockCheck = Mutable{ROM8, 0x4adb, 0x05, 0x02}
-	HoronShopSellCheck  = Mutable{ROM8, 0x48d0, 0x05, 0x02}
+	HoronShopStockCheck = Mutable{0x08, 0x4adb, 0x05, 0x02}
+	HoronShopSellCheck  = Mutable{0x08, 0x48d0, 0x05, 0x02}
 
 	// can replace the gnarled key with a different item
-	MakuDropID    = Mutable{RO15, 0x657d, 0x42, 0x42}
-	MakuDropSubID = Mutable{RO15, 0x6580, 0x00, 0x00}
+	MakuDropID      = Mutable{0x15, 0x657d, 0x42, 0x42}
+	MakuDropSubID   = Mutable{0x15, 0x6580, 0x00, 0x00}
+	MakuRedropID    = Mutable{0x09, 0x7dff, 0x42, 0x42}
+	MakuRedropSubID = Mutable{0x09, 0x7e02, 0x01, 0x01}
+	MakuRedropCheck = Mutable{0x09, 0x7de6, 0x42, 0x42}
 )
