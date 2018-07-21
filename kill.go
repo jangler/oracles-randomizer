c1e4d6e88d4e7b07808c7e4ac5ba36288d4aa4e1
Brandon Mulcahy <brandon@lightcones.net>
2018-07-21 18:41:23 -0400

    Update kill data, testing for all items

--- kill.go
+++ kill.go
@@ -17,6 +17,32 @@ package main
 // route so far that requires killing ropes in a room with pits, there won't be
 // any "kill rope (pit)" node.
 
+// when testing how to kill enemies, remember to try:
+// - sword
+// - beams
+// - boomerang L-1
+// - boomerang L-2
+// - rod
+// - seeds (satchel first, slingshot if satchel doesn't work)
+// - bombs
+// - thrown objects (if applicable)
+// - magnet ball (if applicable)
+// - fool's ore
+// - punch
+// - what pushes them into pits (if applicable)
+//   - sword
+//   - beams
+//   - shield
+//   - NOT boomerangs; they only push if they damage already
+//   - seeds (satchel first, slingshot if satchel doesn't work)
+//   - rod
+//   - bombs
+//   - shovel
+//   - thrown objects (if applicable)
+//   - NOT magnet ball; it kills anything pittable
+//   - fool's ore
+//   - punch
+
 var killNodesAnd = map[string][]string{
 	"slingshot kill normal":   []string{"slingshot", "seed kill normal"},
 	"jump kill normal":        []string{"jump", "kill normal"},
@@ -28,23 +54,22 @@ var killNodesAnd = map[string][]string{
 var killNodesOr = map[string][]string{
 	// enemies in normal route-ish order, but with prereqs first
 	"seed kill normal":          []string{"ember seeds", "scent seeds", "gale seeds", "mystery seeds"},
-	"kill normal":               []string{"sword", "bombs", "fists", "seed kill normal"},
-	"pit kill normal":           []string{"sword", "shield", "bombs", "rod", "shovel", "fists", "scent seeds"},
+	"kill normal":               []string{"sword", "bombs", "beams", "seed kill normal", "fool's ore", "punch"},
+	"pit kill normal":           []string{"sword", "beams", "shield", "scent seeds", "rod", "bombs", "shovel", "fool's ore", "punch"},
 	"kill normal (pit)":         []string{"kill normal", "pit kill normal"},
 	"kill stalfos":              []string{"kill normal", "rod"},
 	"kill stalfos (throw)":      []string{"kill stalfos", "bracelet"},
-	"kill goriya bros":          []string{"sword", "bombs", "fists"},
+	"kill goriya bros":          []string{"sword", "bombs", "fool's ore", "punch"},
 	"kill goriya":               []string{"kill normal"},
 	"kill goriya (pit)":         []string{"kill goriya", "pit kill normal"},
-	"kill aquamentus":           []string{"sword", "bombs", "fists", "scent seeds"},
+	"kill aquamentus":           []string{"sword", "beams", "scent seeds", "bombs", "fool's ore", "punch"},
 	"kill rope":                 []string{"kill normal"},
-	"kill hardhat (pit, throw)": []string{"gale seeds", "sword", "shield", "bombs", "rod", "shovel", "scent seeds", "bracelet"},
-	"kill moblin (gap, throw)":  []string{"sword", "bombs", "fists", "bracelet", "scent seeds", "slingshot kill normal", "jump kill normal"},
-	"kill gel":                  []string{"sword", "bombs", "fists", "ember seeds", "scent seeds", "slingshot gale seeds", "slingshot mystery seeds"},
+	"kill hardhat (pit, throw)": []string{"gale seeds", "sword", "beams", "shield", "scent seeds", "rod", "bombs", "shovel", "fool's ore", "bracelet"},
+	"kill moblin (gap, throw)":  []string{"sword", "beams", "scent seeds", "slingshot kill normal", "bombs", "fool's ore", "punch"},
+	"kill gel":                  []string{"sword", "beams", "ember seeds", "slingshot gale seeds", "slingshot mystery seeds", "bombs", "fool's ore", "punch"},
 	"kill facade":               []string{"bombs"},
 	"kill beetle":               []string{"kill normal"},
-	// TODO: required enemies after d2
 
 	// enemies not required to kill until later
-	"kill moldorm": []string{"sword", "bombs", "fists", "scent seeds"},
+	"kill moldorm": []string{"sword", "bombs", "punch", "scent seeds"},
 }
