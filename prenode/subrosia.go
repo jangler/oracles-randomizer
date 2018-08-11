27fd4f8f2f2dcd959f17b5e8be35a561fa64097a
Brandon Mulcahy <brandon@lightcones.net>
2018-08-11 01:38:11 -0400

    Don't count reaching d8 as a step for progression purposes

--- prenode/subrosia.go
+++ prenode/subrosia.go
@@ -55,7 +55,7 @@ var subrosiaPrenodes = map[string]*Prenode{
 	"autumn tower":       And("temple", "jump", "bomb flower"),
 
 	"eruption room": Or("remains portal"),
-	"enter d8":      OrStep("d8 portal"),
+	"enter d8":      Or("d8 portal"),
 
 	// a few places are unaccounted for, but they're irrelevant for now
 }
