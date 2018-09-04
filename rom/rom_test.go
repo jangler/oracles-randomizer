4643c8a2a01ca0e50e2b2b56eecd4349db746f5d
Brandon Mulcahy <brandon@lightcones.net>
2018-09-03 20:36:21 -0400

    Add unit test for overlapping mutables

--- rom/rom_test.go
+++ rom/rom_test.go
@@ -9,3 +9,23 @@ func TestGraphicsPresent(t *testing.T) {
 		}
 	}
 }
+
+func TestMutableOverlap(t *testing.T) {
+	hitBytes := make(map[int]*string)
+
+	for k, v := range getAllMutables() {
+		switch v := v.(type) {
+		case *MutableRange:
+			for _, addr := range v.Addrs {
+				offset := addr.FullOffset()
+				for i := offset; i < offset+len(v.New); i++ {
+					if hitBytes[i] != nil {
+						t.Errorf("%s collides with %s at %d",
+							k, *hitBytes[i], i)
+					}
+					hitBytes[i] = &k
+				}
+			}
+		}
+	}
+}
