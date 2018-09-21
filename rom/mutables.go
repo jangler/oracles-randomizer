fe5d6fbce526fa92b6a33caed1210a7b1fff302e
Brandon Mulcahy <brandon@lightcones.net>
2018-09-21 00:43:58 -0400

    Use "spawn treasure on link" func in other EOB functions

--- rom/mutables.go
+++ rom/mutables.go
@@ -622,9 +622,8 @@ var constMutables = map[string]Mutable{
 	// shop data (sprite, text) and allows the item to progressively upgrade.
 	"shop spawn item call": MutableWord(Addr{0x08, 0x4bfc}, 0xeb16, 0xc07f),
 	"shop spawn item func": MutableString(Addr{0x08, 0x7fc0}, "\x08",
-		"\xc5\x47\x7d\xcd\xe1\x7f\x78\xc1\x28\x04\xcd\xeb\x16\xc9"+
-			"\xd5\xf5\xcd\xc6\x3a\xf1\x36\x60\x23\x22\x71"+ // spawn item
-			"\x11\x0b\xd0\xcd\x02\x22\xd1\xc9"+ // copy position and ret
+		"\xc5\x47\x7d\xcd\xd2\x7f\x78\xc1\x28\x04\xcd\xeb\x16\xc9"+
+			"\xcd\xd3\x3e\xc9"+ // spawn item and ret
 			"\xfe\xe9\xc8\xfe\xcf\xc8\xfe\xd3\xc8\xfe\xd9\xc9"), // check addr
 	// and zero the original text IDs
 	"zero shop text": MutableStrings([]Addr{{0x08, 0x4d53}, {0x08, 0x4d46},
@@ -636,8 +635,7 @@ var constMutables = map[string]Mutable{
 	"market spawn item func": MutableString(Addr{0x09, 0x7fae}, "\x09",
 		"\xf5\x7d\xfe\xdb\x28\x0f\xfe\xe3\x28\x0b\xfe\xf5\x28\x07"+
 			"\xf1\xcd\xeb\x16\x1e\x42\xc9"+ // do the normal thing if no match
-			"\xcd\xc6\x3a\xf1\x36\x60\x23\x22\x71\xd5\x11"+ // spawn item
-			"\x0b\xd0\xcd\x02\x22\xd1\x37\xc9"), // copy position, scf, ret
+			"\xf1\xcd\xd3\x3e\xd1\x37\xc9"), // spawn item, scf, ret
 
 	// spawn item on top of link in rod cutscene instead of giving it directly.
 	"rod spawn item call": MutableString(Addr{0x15, 0x70cf},
