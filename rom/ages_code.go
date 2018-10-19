e3651f79ce663cd9dc130a583cba4377ae18335d
Brandon Mulcahy <brandon@lightcones.net>
2018-10-19 19:22:41 -0400

    Fix -nomusic function to handle up to sound ID 46 (black tower)

--- rom/ages_code.go
+++ rom/ages_code.go
@@ -24,7 +24,7 @@ func initAgesEOB() {
 	// don't play any music if the -nomusic flag is given. because of this,
 	// this *must* be the first function at the end of bank zero (for now).
 	r.appendToBank(0x00, "no music func",
-		"\x67\xfe\x40\x30\x03\x3e\x08\xc9\xf0\xb7\xc9")
+		"\x67\xfe\x47\x30\x03\x3e\x08\xc9\xf0\xb7\xc9")
 	r.replace(0x00, 0x0c9a, "no music call",
 		"\x67\xf0\xb7", "\x67\xf0\xb7") // modified only by SetNoMusic()
 
--- rom/seasons_code.go
+++ rom/seasons_code.go
@@ -45,7 +45,7 @@ func initSeasonsEOB() {
 	// don't play any music if the -nomusic flag is given. because of this,
 	// this *must* be the first function at the end of bank zero (for now).
 	r.appendToBank(0x00, "no music func",
-		"\x67\xfe\x40\x30\x03\x3e\x08\xc9\xf0\xb5\xc9")
+		"\x67\xfe\x47\x30\x03\x3e\x08\xc9\xf0\xb5\xc9")
 	r.replace(0x00, 0x0c76, "no music call",
 		"\x67\xf0\xb5", "\x67\xf0\xb5") // modified only by SetNoMusic()
 
