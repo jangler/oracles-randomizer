9399e6944111da9558aafd6c08ff989af9e86d9a
Brandon Mulcahy <brandon@lightcones.net>
2018-10-31 13:02:21 -0400

    Skip first two times you have to talk to ghost to get poe clock

--- rom/ages_code.go
+++ rom/ages_code.go
@@ -103,11 +103,13 @@ func initAgesEOB() {
 		"\xe5\xfa\xb3\xcb\xfe\x94\x30\x03\xcd\x86\x08\xe1\xcd\x37\x02\xc9")
 	r.replace(0x03, 0x4d6c, "skip capcom call", "\x37\x02", skipCapcom)
 
-	// set intro flags to skip opening
+	// set intro flags to skip opening, open goron tunnel, and abbreviate poe
+	// clock sequence.
 	skipOpening := r.appendToBank(0x03, "skip opening",
 		"\xcd\xf9\x31\x3e\x0a\xcd\xf9\x31"+ // set global flags
 			"\xe5\x21\x7a\xc7\xcb\xf6\x2e\x6a\xcb\xf6"+ // set room flags
-			"\x2e\x59\xcb\xf6\x2e\x39\x36\xc8\x2e\x0a\xcb\xf6\xe1\xc9") // more
+			"\x2e\x59\xcb\xf6\x2e\x39\x36\xc8\x2e\x0a\xcb\xf6"+ // more
+			"\x2e\x7c\xcb\xf6\x2e\x2e\xcb\xf6\xe1\xc9") // more
 	r.replace(0x03, 0x6e97, "call skip opening",
 		"\xc3\xf9\x31", "\xc3"+skipOpening)
 
