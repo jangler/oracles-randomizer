e558b7ece2e3f39617d8d7f3e54840df3bdcaed6
Brandon Mulcahy <brandon@lightcones.net>
2018-11-19 19:38:46 -0500

    Update mkdist script for checklist and tracker folders

--- mkdist.sh
+++ mkdist.sh
@@ -9,20 +9,19 @@ version="$(git tag --contains HEAD)"
 appname="$(basename "$PWD")"
 
 unix2dos -n README.md README.txt
-unix2dos -n doc/checklist.html checklist.html
 
 mkdir -p "dist/$version"
 GOOS=windows GOARCH=386 go build
 apack "dist/$version/$appname"_win32_"$version.zip" "$appname.exe" README.txt \
-	checklist.html
+	checklist/ tracker/
 GOOS=darwin GOARCH=amd64 go build
 apack "dist/$version/$appname"_macos64_"$version.zip" "$appname" README.txt \
-	checklist.html
+	checklist/ tracker/
 GOOS=linux GOARCH=amd64 go build
 apack "dist/$version/$appname"_linux64_"$version.zip" "$appname" README.txt \
-	checklist.html
+	checklist/ tracker/
 
-rm README.txt checklist.html
+rm README.txt
 
 echo "============================="
 echo "MAKE SURE TO UPDATE VERSION!!"
