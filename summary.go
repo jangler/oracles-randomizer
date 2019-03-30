90b1cf17ff39c4287e9350fb6ac107fb1d3a52d5
Brandon Mulcahy <brandon@lightcones.net>
2019-03-29 20:37:46 -0400

    Update version number to 3.4.0-alpha4

--- summary.go
+++ summary.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-const version = "3.4.0-alpha3"
+const version = "3.4.0-alpha4"
 
 // returns a channel that will write strings to a text file with CRLF line
 // endings. the function will send on the int channel when finished printing.
