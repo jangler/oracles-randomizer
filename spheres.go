de8160d6c51c2e0adbeb8b86b66e196c0dc57b2a
Brandon Mulcahy <brandon@lightcones.net>
2019-05-16 19:07:47 -0400

    Fix sphere/log things
    - Don't add duplicate parents to keys in sphere analysis
    - Don't truncate season area names redundantly
    - List unreachable items at the end of each section

--- spheres.go
+++ spheres.go
@@ -21,19 +21,20 @@ func getChecks(ri *RouteInfo) map[*node]*node {
 // getSpheres returns successive slices of nodes that can be reached at a step
 // in item collection. sphere 0 is the nodes that can be reached from the
 // start with no items; sphere 1 is the nodes that can be reached using the
-// items from sphere 0, and so on. each node only belongs to one sphere.
-func getSpheres(g graph, checks map[*node]*node, hard bool) [][]*node {
+// items from sphere 0, and so on. each node only belongs to one sphere. it
+// also returns a separate slice of nodes that aren't reachable at all.
+func getSpheres(g graph, checks map[*node]*node, hard bool) ([][]*node, []*node) {
 	reached := make(map[*node]bool)
 	spheres := make([][]*node, 0)
 
 	// need to track unreached items so that unreached dungeon items etc can
 	// have their parents restored even if they're not reachable yet.
-	unreached := make(map[*node]*node)
+	unreachedChecks := make(map[*node]*node)
 	for slot, item := range checks {
 		// don't delimit spheres by intra-dungeon keys -- it obscured "actual"
 		// progression in the log file.
 		if !keyRegexp.MatchString(item.name) {
-			unreached[slot] = item
+			unreachedChecks[slot] = item
 			item.removeParent(slot)
 		}
 	}
@@ -52,9 +53,11 @@ func getSpheres(g graph, checks map[*node]*node, hard bool) [][]*node {
 		// mark nodes as reached and add item checks into the next iteration
 		for _, n := range sphere {
 			reached[n] = true
-			delete(unreached, n)
 			if item := checks[n]; item != nil {
-				item.addParent(n)
+				if unreachedChecks[n] != nil {
+					delete(unreachedChecks, n)
+					item.addParent(n)
+				}
 				sphere = append(sphere, item)
 				reached[item] = true
 			}
@@ -66,17 +69,24 @@ func getSpheres(g graph, checks map[*node]*node, hard bool) [][]*node {
 		spheres = append(spheres, sphere)
 	}
 
-	for slot, item := range unreached {
+	for slot, item := range unreachedChecks {
 		item.addParent(slot)
 	}
 
-	return spheres
+	extra := make([]*node, 0)
+	for slot, item := range checks {
+		if !reached[slot] {
+			extra = append(extra, slot, item)
+		}
+	}
+
+	return spheres, extra
 }
 
 // logSpheres prints item placement by sphere to the summary channel.
 func logSpheres(summary chan string, checks map[*node]*node,
-	spheres [][]*node, game int, filter func(string) bool) {
-	for i, sphere := range spheres {
+	spheres [][]*node, extra []*node, game int, filter func(string) bool) {
+	for i, sphere := range append(spheres, extra) {
 		// get lines first, to make sure there are actual relevant items in
 		// this sphere.
 		lines := make([]string, 0)
@@ -96,7 +106,11 @@ func logSpheres(summary chan string, checks map[*node]*node,
 
 		// then log the sphere if it's non-empty.
 		if len(lines) > 0 {
-			summary <- fmt.Sprintf("sphere %d:", i)
+			if i < len(spheres) {
+				summary <- fmt.Sprintf("sphere %d:", i)
+			} else {
+				summary <- "inaccessible:"
+			}
 			sort.Strings(lines)
 			for _, line := range lines {
 				summary <- line
