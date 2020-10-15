e8db42db3b900b3790afa4b90ff143ce47604ece
Brandon Mulcahy <brandon@lightcones.net>
2020-10-15 15:11:02 -0400

    Add generated files to repo
    This makes the repo `go get`-able by fyne-cross

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+// Code generated - DO NOT EDIT.
+
+import "github.com/jangler/oracles-randomizer/randomizer"
+
+func main() {
+	randomizer.Main()
+}
---
+++ randomizer/version.go
@@ -0,0 +1,5 @@
+package randomizer
+
+// Code generated - DO NOT EDIT.
+
+const version = "4.1.3"
